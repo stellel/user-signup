56763163e6188e769f2a675ec165169ceafaf1d0
stellel <john@doe.org>
2017-02-07 07:24:18 -0500

    password errors updated

--- main.py
+++ main.py
@@ -47,10 +47,7 @@ def valid_username(username):
 
 EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
 def valid_email(email):
-	if email == "":
-		return True
-
-	return EMAIL_RE.match(email)
+	return not email or email.strip() == "" or EMAIL_RE.match(email)
 
 
 class SignupForm(webapp2.RequestHandler):
@@ -66,7 +63,7 @@ class SignupForm(webapp2.RequestHandler):
 		</label>"""
 
 		#if we have an error make a <p> to display it
-		error = self.request.get("error")
+		error = cgi.escape(self.request.get("error"))
 		error_element = "<p class='error'>" + error + "</p>" if error else ""
 		
 		form_password = """
@@ -74,12 +71,17 @@ class SignupForm(webapp2.RequestHandler):
 		    Password
 		    <input type="text" name="password" />
 		</label></p>
+		"""
+		no_password_error = cgi.escape(self.request.get("no_password_error"))
+		no_password_error_element = "<p class='error'>" + no_password_error + "</p>" if no_password_error else ""
+
+		form_password_confirm = """
 		<p>
 		<label>Confirm Password
 		    <input type="text" name="verify" />
 		</label></p>
 		"""
-		password_error = self.request.get("password_error")
+		password_error = cgi.escape(self.request.get("password_error"))
 		password_error_element = "<p class='error'>" + password_error + "</p>" if password_error else ""
 		
 		form_email = """
@@ -88,43 +90,65 @@ class SignupForm(webapp2.RequestHandler):
 		    <input type="text" name="email" />
 		</label></p>
 		"""
-		email_error = "<p class='error'>" + self.request.get("email_error") + "</p>" if error else ""
+		email_error = "<p class='error'>" + cgi.escape(self.request.get("email_error")) + "</p>" if error else ""
 		
 		form_submit = """
 		<p><input type="submit" /></p>
 		</form>
 		"""
 
-		main_content = signup_header + form_name + error_element + form_password + password_error_element + form_email + email_error + form_submit
+		main_content = signup_header + form_name + error_element + form_password + no_password_error_element + form_password_confirm + password_error_element + form_email + email_error + form_submit
 		page_content = page_header + main_content + page_footer
 		self.response.write(page_content)
 
 class ConfirmSubmission(webapp2.RequestHandler):
 	"""handles requests coming in to /confirm"""
 	def post(self):
+		hasError = False
 		username = self.request.get("username")
 		password = self.request.get("password")
 		verify = self.request.get("verify")
 		email =  self.request.get("email")
+		url = "/?"
 
-		if not username:
-			# make a helpful error message
-			error = "Please enter a user name."
-			error_blank = cgi.escape(error, quote=True)
 			# redirect to homepage, and include error as a query parameter in the URL
-			self.redirect("/?error=" + error_blank)
+			
 
-		elif not valid_username(username):
+		if not username or not valid_username(username):
 			error = "That is not a valid user name."
-			self.redirect("/?error=" + error)
+			hasError = True
+			url += "error=That is not a valid user name&" 
 
-		elif password != verify:
+		else:
+			url += ""
+
+		if not password:
+			no_password_error = "Please enter a password."
+			hasError = True
+			url += "no_password_error=Please enter a password&"
+		
+		else:
+			url += ""
+
+		if password != verify:
 			password_error = "The passwords don't match."
-			self.redirect("/?error=" + password_error)
+			hasError = True
+			url += "password_error=The passwords don't match&"
+		
+		else:
+			url += ""
 
-		elif not valid_email(email):
+		if not valid_email(email):
 			email_error = "That is not a valid email address."
-			self.redirect("/?error=" + email_error)
+			hasError = True
+			url += "email_error=That is not a valid email address&"
+
+		else: 
+			url += ""
+
+
+		if hasError:	
+			self.redirect(url)
 
 		confirmation_message = "Welcome, " + username + "!"
 		confirmation = page_header + "<p>" + confirmation_message + "</p>" + page_footer
